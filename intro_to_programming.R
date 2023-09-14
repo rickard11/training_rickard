3af3f0f6b9bbf79d94934ae6641f39d3b9ff99d1
rickard11! <rickard@ucsb.edu>
2023-09-14 10:32:54 -0700

    Initial commit

---
+++ intro_to_programming.R
@@ -0,0 +1,28 @@
+weight_kg <- c(55,25,12)
+
+#convert weight to lbs
+2.2 * weight_kg
+
+
+#Set relative directory ----
+# 4 dashes adds outlione (topright of source view)
+#tab pulls in top csv file
+
+chem<-read.csv("data/BGchem2008data.csv")
+
+# run these in console str(chem), View(chem)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ notes.R
@@ -0,0 +1,39 @@
+#relative path can be used to share code and for reproducability
+#absolute paths can cause crash on a different computer.
+
+#git comands written in the Terminal
+
+#Terminal commands to connect r with git
+
+#rickard@included-crab:~/training_rickard$ git config --global user.name "rickard11!"
+#rickard@included-crab:~/training_rickard$ git config --global user.email "rickard@ucsb.edu"
+#rickard@included-crab:~/training_rickard$ git config --global credential.helper 'cache --timeout=10000000'
+#rickard@included-crab:~/training_rickard$ git config --global init.defaultBranch main
+#rickard@included-crab:~/training_rickard$ git config --global --list
+
+
+#in console
+
+#usethis::create_github_token()
+#opens web browser. name it and copy the token
+#gitcreds::gitcreds_set()
+#Enter password or token: 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
+
+#console and right hand corner tells you your working directory
+
+
+#-------------------------------------------------------------------------------#
+#data management plans
+#data one
+
+
+
+
+#####################
+#Git
+#git merge brings data down to your working file, git fetch makes it so you can see the data, but not merge with your working file
+
+
+
+
+
